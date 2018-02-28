c949e453b5b1706ac5d12cb48775862c9b86509c
Adrian DeRose <adrian.derose@zumer.io>
2018-02-28 14:56:47 -0800

    Set background image and background overlay

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-$(document).ready(function() {
+function changeNavOpacity() {
+    $(document).ready(function() {
         // Transition effect for navbar 
         $(window).scroll(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 150) { 
-              $('.navbar').addClass('solid');
+          if($(this).scrollTop() > 50) { 
+              $('#top-nav').addClass('solid');
           } else {
-              $('.navbar').removeClass('solid');
+              $('#top-nav').removeClass('solid');
           }
         });
+    });
+}
+
+function smoothLink() {
+    $('.navigation-menu a').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}
+
+changeNavOpacity();
+smoothLink();
+
+
 
         
-});
