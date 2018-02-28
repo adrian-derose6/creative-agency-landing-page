2bc3e986e5eba758efdff064cbe0b0774808fd29
Adrian DeRose <adrian.derose@zumer.io>
2018-02-28 15:59:41 -0800

    Correct nav link colors and opacity

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 function changeNavOpacity() {
     $(document).ready(function() {
+        $navLink = $('.navigation-menu > li > a');
         // Transition effect for navbar 
         $(window).scroll(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
           if($(this).scrollTop() > 50) { 
               $('#top-nav').addClass('solid');
+              $navLink.addClass('colored-link');
+              $('.logo').addClass('colored-logo')
           } else {
               $('#top-nav').removeClass('solid');
+              $navLink.removeClass('colored-link');
+              $('.logo').removeClass('colored-logo');
           }
         });
     });
