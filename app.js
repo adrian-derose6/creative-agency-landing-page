5382a6146285bfabce0b366c75427495cbd97d40
Adrian DeRose <adrian.derose@zumer.io>
2018-02-28 18:29:36 -0800

    Add all of the page sections

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ function changeNavOpacity() {
     });
 }
 
-function smoothLink() {
-    $('.navigation-menu a').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 0
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-}
 
 changeNavOpacity();
 smoothLink();
