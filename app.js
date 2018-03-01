6ec5d271878f2733bc44a3a88b652fb2ff361b74
Adrian DeRose <adrian.derose@zumer.io>
2018-02-28 20:28:59 -0800

    Moved app.js script tag to bottom of <body>

--- app.js
+++ app.js
@@ -17,9 +17,56 @@ function changeNavOpacity() {
     });
 }
 
+function getPosition(el) {
+  var xPos = 0;
+  var yPos = 0;
+ 
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+ 
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+ 
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
+}
+
+function scrollSpy() {
+    var sectionsList = document.getElementsByTagName('section');
+    var currentSection = sectionsList[1];
+
+    console.log(sectionsList[0]);
+
+
+   //window.onscroll = function() { 
+       // let scrollPosition = document.body.scrollTop;
+
+
+    //};  
+}
+
+
+// Check current scroll position -- 
+// Check whether scroll is in section field
+// 
+
+
 
 changeNavOpacity();
-smoothLink();
+scrollSpy();
+
 
 
 
