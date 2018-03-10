7975212bc00c7d831e67468d4fa1abe806abd7b8
Adrian DeRose <adrian.derose@zumer.io>
2018-03-10 02:14:59 -0800

    Slider returns to beginning when end is reached; arrows positioned dynamically

--- js/carousel.js
+++ js/carousel.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
 
   $('.carousel').slick({
       accessibility: true,
-      autoplay: true,
-      autoplaySpeed: 5000,
       lazyLoad: 'ondemand',
       infinite: false,
       focusOnSelect: false,
+      nextArrow: $('.arrow-next'),
+      prevArrow: $('.arrow-prev'),
+      appendArrows: $('.carousel'),
       responsive: [
         {
           breakpoint: 1500,
@@ -32,4 +33,48 @@ $(document).ready(function() {
         }
       ]
   });
+    
+  var photo = $('.carousel');
+  var offset = photo.offset();
+  var photoViewportOffsetTop = offset.top - $(document).scrollTop();
+  var carouselViewportOffsetLeft = offset.left - $(document).scrollLeft();
+
+  $('.arrow-next').css("right", carouselViewportOffsetLeft + $('.arrow-next').outerWidth() / 2);
+  $('.arrow-prev').css("left", carouselViewportOffsetLeft +  $('.arrow-prev').outerWidth() / 2);
+
+  $(window).resize(function() {
+      let offset = photo.offset();
+      let photoViewportOffsetTop = offset.top - $(document).scrollTop();
+      let carouselViewportOffsetLeft = offset.left - $(document).scrollLeft();
+
+      $('.arrow-next').css("right", carouselViewportOffsetLeft + $('.arrow-next').outerWidth() / 2);
+      $('.arrow-prev').css("left", carouselViewportOffsetLeft +  $('.arrow-prev').outerWidth() / 2);
+  })
+  
+});
+
+document.querySelector('.arrow-next').addEventListener('click', function() {
+  console.log($('.carousel').slick('slickCurrentSlide'));
+  console.log($('.team-box').length)
+
+  if ($(window).width() < 992 && $('.carousel').slick('slickCurrentSlide') === $('.team-box').length / 2 + 1) {
+    $('.carousel').slick('goTo', 0);
+  }
+
+  else if ($(window).width() < 1500 && $(window).width() > 992 && $('.carousel').slick('slickCurrentSlide') === $('.team-box').length / 2) {
+    $('.carousel').slick('goTo', 0);
+  }
+});
+
+document.querySelector('.arrow-prev').addEventListener('click', function() {
+  console.log($('.carousel').slick('slickCurrentSlide'));
+  console.log($('.team-box').length);
+
+  if ($(window).width() < 992 && $('.carousel').slick('slickCurrentSlide') === $('.team-box').length / 2 + 1) {
+    $('.carousel').slick('goTo', 0);
+  }
+
+  else if ($(window).width() < 1500 && $(window).width() > 992 && $('.carousel').slick('slickCurrentSlide') === 0) {
+    $('.carousel').slick('goTo', $('.team-box').length / 2);
+  }
 });
